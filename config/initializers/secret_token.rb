4bf42d3081278ceb9ec300d05b0a6018cff50bc1
Rocky SIGNAVONG <admin@yourenglishsolution.com>
2011-01-19 15:46:22 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Microlearning::Application.config.secret_token = 'e5d72abab439358fd20526a81aacc2d217624b2d17049f2ddf77f552274114fb1aafbcfb3dddd3e7929c598699c8de1528da046b68a1e69166378ab5bc508163'
